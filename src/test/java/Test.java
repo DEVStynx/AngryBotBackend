50c4bf1e54517a9f7fdbc2cd54decd76c772d6b8
Jonas <131193709+DEVStynx@users.noreply.github.com>
2025-01-21 10:50:23 +0100

    Update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -2,6 +2,6 @@ import de.stynxyxy.angrybot.networking.objects.FileTransferServer;
 
 public class Test {
     public static void main(String[] args) {
-        FileTransferServer server = new FileTransferServer("C:\\Users\\jonas\\Desktop\\Development\\java\\AngryBot\\AngryBotClient\\picture.png",450);
+        FileTransferServer server = new FileTransferServer("C:\\Users\\Desktop\\Development\\java\\AngryBot\\AngryBotClient\\picture.png",450);
     }
 }
